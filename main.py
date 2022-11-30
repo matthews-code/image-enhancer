86a9990a9e20a3a00efb133c85ec3e274968d9c3
matthews-code <matthew_buensalida@dlsu.edu.ph>
2022-11-30 17:46:58 +0800

    Modified main.py

--- main.py
+++ main.py
@@ -10,22 +10,6 @@ sharedImages = []
 sharedResourceBuffer = []
 semaphoreBuffer = threading.Semaphore(0)
 
-def enhanceImage(image, eBrightness, eSharpness, eContrast):    
-    # Enhance brightness
-    currBrightness      = ImageEnhance.Brightness(image)
-    imageBrightness     = currBrightness.enhance(eBrightness)
-
-    # Enhance sharpness
-    currSharpness       = ImageEnhance.Sharpness(imageBrightness)
-    imageSharpness      = currSharpness.enhance(eSharpness)
-
-    # Enhance contrast
-    currContrast        = ImageEnhance.Contrast(imageSharpness)
-    finalImage          = currContrast.enhance(eContrast)
-
-    # finalImage.show()
-    finalImage.save('enhanced/gfg-enhanced.png')
-
 class producer (threading.Thread):
     def __init__(self, images):
         threading.Thread.__init__(self)
@@ -34,21 +18,23 @@ class producer (threading.Thread):
     def run(self):
         global sharedResourceBuffer
         global semaphoreBuffer
-        print('Producer is initializing.\n')
-        time.sleep(2)
+        print('\nProducer is initializing.')
         for img in self.images:
             sharedResourceBuffer.append(img)
             semaphoreBuffer.release()
-            print('Producer appended an image.\n')
-            time.sleep(2)
+            print('Producer appended an image.')
 
 class consumer (threading.Thread):
-    def __init__(self, images, threadId):
+    def __init__(self, images, bF, cF, sF, threadId):
         threading.Thread.__init__(self)
         self.counter = len(images)
+        self.brightness = bF
+        self.contrast = cF
+        self.sharpness = sF
         self.id = threadId
         self.list = []
         self.item = 0
+
     def run(self):
         global sharedResourceBuffer
         global semaphoreBuffer
@@ -56,25 +42,35 @@ class consumer (threading.Thread):
         for i in range(self.counter):
             semaphoreBuffer.acquire() 
             if sharedResourceBuffer:
-                self.item = sharedResourceBuffer.pop()
+                origImg = sharedResourceBuffer.pop()
+                self.item = self.applyEffects(origImg)
 
-                print('Apply brightness')
-                print('Apply contrast')
-                print('Apply sharpness')
+                self.item.save('enhanced/' + str(i) + '.png')
 
                 self.list.append(self.item)
-                print("Consumer %i popped an item \n"%(self.id)) 
+                print("Consumer %i popped an image"%(self.id))
 
-            time.sleep(2)
         print('The values acquired by consumer ' + str(self.id) + ' are ' + str(self.list) + '\r\n')
+
+    def applyEffects(self, image):    
+        currBrightness      = ImageEnhance.Brightness(image)
+        currImage     = currBrightness.enhance(self.brightness)
+
+        currContrast        = ImageEnhance.Contrast(currImage)
+        currImage          = currContrast.enhance(self.contrast)
+        
+        currSharpness       = ImageEnhance.Sharpness(currImage)
+        finalImage      = currSharpness.enhance(self.sharpness)
+
+        return finalImage
 
 if __name__ == "__main__":
     # Start program
     print('Running Image Enhancer!')
     
     # Take inputs
-    folderImages    = input('Folder name of input images: ')
-    folderEnhanced  = input('Folder name of output images: ')
+# folderImages    = input('Folder name of input images: ')
+# folderEnhanced  = input('Folder name of output images: ')
 
     # Example inputs: 2.5, 8.3, 0.3
     bF = float(input('Brightness Factor: '))
@@ -86,12 +82,20 @@ if __name__ == "__main__":
     files = os.listdir(path)
 
     for image in files:
-        img = Image.open(image)
+        img = Image.open(path + '/' + image)
         sharedImages.append(img)
 
     # Create threads
     producerThread = producer(sharedImages)
-    consumerThread = consumer(sharedImages, 1)
+    consumerThread = consumer(sharedImages, bF, cF, sF, 1)
+
+    startTime = time.time()
+
+    producerThread.start()
+    consumerThread.start()
+
+    producerThread.join()
+    consumerThread.join()
 
     # When finished running all processes
-    print('Done!')
+    print("--- %s seconds ---" % (time.time() - startTime))
