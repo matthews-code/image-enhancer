3d68e7f887f633731b44458fa163044ce85ec7b2
Darvvvin <jacobisdarvin@gmail.com>
2022-12-01 21:49:23 +0800

    asdasd

--- main.py
+++ main.py
@@ -19,16 +19,17 @@ class producer (threading.Thread):
     def run(self):
         global sharedResourceBuffer
         global semaphoreBuffer
-        
+
         i = self.id * self.counter
         for i in range(self.id * self.counter, (self.id * self.counter) + self.counter):
             sharedResourceBuffer.append(self.images[i])
             semaphoreBuffer.release()
 
+
 class consumer (threading.Thread):
     def __init__(self, images, brightness, contrast, sharpness, threadId):
         threading.Thread.__init__(self)
-            
+
         self.brightness = brightness
         self.contrast = contrast
         self.sharpness = sharpness
@@ -43,13 +44,13 @@ class consumer (threading.Thread):
         global semaphoreBuffer
 
         for i in range(self.counter):
-            semaphoreBuffer.acquire() 
+            semaphoreBuffer.acquire()
             if sharedResourceBuffer:
                 origImg = sharedResourceBuffer.pop()
                 finalImage = self.applyEffects(origImg[0])
                 finalImage.save(destinationPath + origImg[1])
 
-    def applyEffects(self, image):    
+    def applyEffects(self, image):
         currBrightness = ImageEnhance.Brightness(image)
         currImage = currBrightness.enhance(self.brightness)
         currContrast = ImageEnhance.Contrast(currImage)
@@ -59,6 +60,7 @@ class consumer (threading.Thread):
         return finalImage
 
 # ------- Global variables -------
+
 
 sharedImages = []
 sharedResourceBuffer = []
@@ -76,45 +78,65 @@ enhanceTime = 0
 
 if __name__ == "__main__":
     # ------- Start program -------
-    print('Running Image Enhancer!')
-    
+    print('\nRunning Image Enhancer!\n')
+    print('||===========================||\n')
     # ------- Take path inputs -------
-    sourcePath    = input('Folder name of input images [Leave blank for `images`]: ')
-    destinationPath  = input('Folder name of output images [Leave blank for `enhanced`]: ')
+    sourcePath = input(
+        'Folder name of input images    [Leave blank for `images`]: ')
+    destinationPath = input(
+        'Folder name of output images   [Leave blank for `enhanced`]: ')
 
-    if (sourcePath == ''): sourcePath = './images'
-    else: sourcePath = './' + sourcePath
+    if (sourcePath == ''):
+        sourcePath = './images'
+    else:
+        sourcePath = './' + sourcePath
 
-    if (destinationPath == ''): destinationPath = 'enhanced'
-    else: os.mkdir(destinationPath) 
-        
+    if (destinationPath == ''):
+        destinationPath = 'enhanced'
+    else:
+        os.mkdir(destinationPath)
+
     destinationPath = './' + destinationPath + '/'
 
     # ------- Take enhancing time input -------
-    enhanceTime = input('Enhance time in minutes [Leave blank for 0.1 minute]: ')
+    enhanceTime = input(
+        'Enhance time in minutes        [Leave blank for 0.1 minute]: ')
 
-    if enhanceTime: enhanceTime = float(enhanceTime) * 60
-    else: enhanceTime = 0.1 * 60
+    if enhanceTime:
+        enhanceTime = float(enhanceTime) * 60
+    else:
+        enhanceTime = 0.1 * 60
 
     # ------- Take factor inputs -------
-    bF = input('Brightness Factor [Leave blank for a factor of 1]: ')
-    cF = input('Contrast Factor [Leave blank for a factor of 1]: ')
-    sF = input('Sharpess Factor [Leave blank for a factor of 1]: ')
+    bF = input('Brightness Factor              [Leave blank for a factor of 1]: ')
+    cF = input('Contrast Factor                [Leave blank for a factor of 1]: ')
+    sF = input('Sharpess Factor                [Leave blank for a factor of 1]: ')
 
-    if bF: bF = float(bF)
-    else: bF = 1
+    if bF:
+        bF = float(bF)
+    else:
+        bF = 1
 
-    if cF: cF = float(cF)
-    else: cF = 1
+    if cF:
+        cF = float(cF)
+    else:
+        cF = 1
 
-    if sF: sF = float(sF)
-    else: sF = 1
+    if sF:
+        sF = float(sF)
+    else:
+        sF = 1
 
     # ------- Take number of thread input -------
-    numConsumerThreads = input('Number of consumer threads [Leave blank for 1 consumer]: ')
+    numConsumerThreads = input(
+        'Number of consumer threads     [Leave blank for 1 consumer]: ')
 
-    if numConsumerThreads: numConsumerThreads = int(numConsumerThreads)
-    else: numConsumerThreads = 1
+    print('\n||===========================||')
+
+    if numConsumerThreads:
+        numConsumerThreads = int(numConsumerThreads)
+    else:
+        numConsumerThreads = 1
 
     # # ------- Load Images -------
     files = os.listdir(sourcePath)
@@ -136,6 +158,12 @@ if __name__ == "__main__":
         consumerThreadList.append(consumerThread)
         consumerThread.start()
 
+    time.sleep(1.5)
+    # Kill thread here
+
+    for consThread in consumerThreadList:
+        consThread.join()    
+
     for prodThread in producerThreadList:
         prodThread.join()
 
@@ -143,4 +171,21 @@ if __name__ == "__main__":
         consThread.join()
 
     # When finished running all processes
-    print("\n--- %s seconds with %i producer/s and %i consumer/s ---" % (time.time() - startTime, numProducerThreads, numConsumerThreads))
+    seconds = (time.time() - startTime)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+
+    lines = [
+        'Program Stats',
+        '| ==================================== |',
+        "Compile Time: %d Minutes & %f Seconds" % (minutes, seconds),
+        "Number of Images: %d" % len(sharedImages),
+        "Output Folder: %s" % destinationPath
+    ]
+
+    with open('stats.txt', 'w') as f:
+        f.write('\n'.join(lines))
+
+    print("\nCompile Time: %d Minutes & %f Seconds" % (minutes, seconds))
+    print("Success! See details in stats.txt.\n")
